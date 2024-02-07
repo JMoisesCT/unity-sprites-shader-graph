c15c2ada963ac50c06981b3734d5eb21562cd6ea
Moisés Cumpa <moises.cumpa@gmail.com>
2024-02-07 00:27:19 -0500

    Update editor script to control radial shader

--- Assets/Examples/RadialFill/Editor/RadialFillControllerEditor.cs
+++ Assets/Examples/RadialFill/Editor/RadialFillControllerEditor.cs
@@ -7,10 +7,14 @@ namespace MoisesCT.SpritesShaderGraph.RadialFill
     public class RadialFillControllerEditor : Editor
     {
         SerializedProperty fill;
+        SerializedProperty clockwise;
+        SerializedProperty originAngle;
 
         private void OnEnable()
         {
             fill = serializedObject.FindProperty("_fill");
+            clockwise = serializedObject.FindProperty("_clockwise");
+            originAngle = serializedObject.FindProperty("_originAngle");
         }
 
         public override void OnInspectorGUI()
@@ -23,6 +27,18 @@ namespace MoisesCT.SpritesShaderGraph.RadialFill
             if (EditorGUI.EndChangeCheck())
             {
                 radialFill.UpdateFillAmount();
+            }
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(clockwise);
+            if (EditorGUI.EndChangeCheck())
+            {
+                radialFill.UpdateClockwise();
+            }
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(originAngle);
+            if (EditorGUI.EndChangeCheck())
+            {
+                radialFill.UpdateOriginAngle();
             }
             serializedObject.ApplyModifiedProperties();
         }
